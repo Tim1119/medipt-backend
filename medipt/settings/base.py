43547a8cbdaa69ced0ddcb3eea0804eaee4de7c0
Ajani Timothy <timothyinioluwa@gmail.com>
2025-06-05 18:55:19 +0100

    fix: changed sttaic files settings

--- medipt/settings/base.py
+++ medipt/settings/base.py
@@ -145,12 +145,10 @@ STATIC_URL = '/static/'
 # STATIC_ROOT = BASE_DIR / "staticfiles"
 
 if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+else:
+    STATICFILES_DIRS = [BASE_DIR / "static"]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_DIRS = [BASE_DIR / "static",]
