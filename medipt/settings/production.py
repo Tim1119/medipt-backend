84356043b1fd3ce981bdbb2856d563c0c90e0c7a
Ajani Timothy <timothyinioluwa@gmail.com>
2025-06-05 21:40:51 +0100

    fix: adde redis public url directly to file

--- medipt/settings/production.py
+++ medipt/settings/production.py
@@ -76,8 +76,8 @@ LOGGING = {
 # CELERY_BROKER_URL = "redis://localhost:6379/0"
 # CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
 
-CELERY_BROKER_URL = env('CELERY_BROKER_URL')
-CELERY_RESULT_BACKEND = env('CELERY_RESULT_BACKEND')
+CELERY_BROKER_URL=redis://default:FszoolJkRjqAFZXieycVFIEazGFgPCKF@ballast.proxy.rlwy.net:27731
+CELERY_RESULT_BACKEND=redis://default:FszoolJkRjqAFZXieycVFIEazGFgPCKF@ballast.proxy.rlwy.net:27731
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_EXPIRES = 3600  # Task state expires after 1 hour
