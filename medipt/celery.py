c199b1c93f60a4e7318d1dcff1ce7902d6ee72ce
Ajani Timothy <timothyinioluwa@gmail.com>
2025-06-13 11:27:06 +0100

    fix: added redis ssl

--- medipt/celery.py
+++ medipt/celery.py
@@ -2,8 +2,10 @@
 
 from __future__ import absolute_import, unicode_literals
 import os
+import ssl
 from celery import Celery
 from django.conf import settings
+from urllib.parse import urlparse
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medipt.settings.production')
@@ -15,6 +17,16 @@ app = Celery('medipt')
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+broker_url = app.conf.broker_url
+
+result_backend = app.conf.result_backend
+
+
+if urlparse(broker_url).scheme == 'rediss':
+    ssl_opts = {'ssl_cert_reqs': ssl.CERT_NONE}
+    app.conf.broker_use_ssl = ssl_opts
+    app.conf.redis_backend_use_ssl = ssl_opts
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
